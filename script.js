62a8f45eff28a7fe5ac770e37da5a1ae46d9156f
aschalew-mamo <162134580+aschalew-mamo@users.noreply.github.com>
2024-09-02 14:50:33 +0300

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar");
-  if (window.scrollY > 0) {
+  if (window.scrollY > 1) {
     navbar.classList.add("menu-scrolled");
   } else {
     navbar.classList.remove("menu-scrolled");
